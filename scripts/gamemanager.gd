1169de2fd68a5ace58acb667a0109779bfafc946
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-11-27 17:19:13 +0100

    Most basic playable first level

---
+++ scripts/gamemanager.gd
@@ -0,0 +1,48 @@
+extends Node
+
+func _ready():
+	randomize()
+
+var mapTileSize := Vector2(250, 250)
+
+var mapTiles := [
+	{
+		"name": "grass",
+		"imageSrc": "res://assets/levels/tiles/tileGrass_.png"
+	},
+	{
+		"name": "destination",
+		"imageSrc": "res://assets/levels/tiles/dileDestination.png"
+	},
+	{
+		"name": "hay",
+		"imageSrc": "res://assets/levels/tiles/tileHay.png"
+	},
+	{
+		"name": "water",
+		"imageSrc": "res://assets/levels/tiles/tileWater.png"
+	},
+	{
+		"name": "mud",
+		"imageSrc": "res://assets/levels/tiles/tileMud.png"
+	}
+]
+
+var mapDimensions := Vector2(6, 8)
+
+var slectedCowIdx := -1
+
+enum directions {STANDING, UP, DOWN, LEFT, RIGHT}
+
+var levels := {
+	0: [
+		["cow", "", "", "", "hay", ""],
+		["", "", "", "", "", ""],
+		["", "", "", "", "", ""],
+		["", "", "", "", "", ""],
+		["", "", "", "", "", ""],
+		["", "", "", "", "", ""],
+		["", "", "", "destination", "", ""],
+		["", "", "", "", "", ""],
+	]
+}
